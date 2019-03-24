10488e911f0501ac1f88860afa495de2dc6f5f6b
Koby Reid <kreidpac@gmail.com>
2019-03-23 23:12:37 -0700

    Add Jacobian to ffs

--- Limb Dynamics.py
+++ Limb Dynamics.py
@@ -7,6 +7,20 @@ from Symbolic_Matricies import J
 expr = sym.sympify(J(2))
 
 
-J = J(2).subs({'q_1': sym.rad(135), 'q_2': sym.rad(-120), 'l_1': 0.254, 'l_2': 0.305}).evalf().tolist()
-J = np.array(J)
-print(J)
+
+def ffs(q_1, q_2, l_1, l_2, R, maxmotorforce):
+
+    # Creating the numerical Jacobian for the system J(2) is for a 2 joint, 2 link system
+    J_num = J(2).subs({'q_1': sym.rad(q_1), 'q_2': sym.rad(q_2), 'l_1': l_1 , 'l_2': l_2}).evalf().tolist()
+    J_mat = np.array(J_num, dtype= 'float')
+    J_square = J_mat[:2, :2] # Reduce the Jacobian to a square matrix
+
+    # Creating Inverse Transpose of the Jacobian
+    J_inv = np.linalg.inv(J_square)
+    J_inv_T = J_inv.T
+    print(J_inv_T)
+
+
+ffs(135, -50, .234, .23562, 1, 1)
+
+
