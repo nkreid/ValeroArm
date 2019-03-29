a045f232781f9f1a9a2d87c6a9ec5ddd87ce80da
Koby Reid <kreidpac@gmail.com>
2019-03-28 20:57:30 -0700

    incomplete circle function

---
+++ LargestCircle.py
@@ -0,0 +1,21 @@
+# This function will find the largest circle in the FFS
+import numpy as np
+
+
+def LargestCircle(hull, x_center, y_center):
+    space = np.linspace(0, 2*np.pi)
+    circ = np.array([np.cos(space), np.sin(space)])
+    r = 0
+    step = 0.0005
+    inHull = True
+
+    while inHull = True:
+
+        points = r * circ
+
+
+
+
+
+
+
