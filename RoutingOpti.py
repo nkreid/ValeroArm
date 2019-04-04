8ca96674d75fe13d8c7a7d7c39650da16df26399
Koby Reid <kreidpac@gmail.com>
2019-04-03 22:27:00 -0700

    began prodicing possible routing

---
+++ RoutingOpti.py
@@ -0,0 +1,16 @@
+import numpy as np
+import itertools
+
+
+def poss_rows(value_list, length):
+    row = np.array(list(itertools.product(value_list, repeat=length)))
+    row_del = []
+    for i in range(len(row)):
+        if -1 in row[i] and 1 in row[i]:
+            pass
+        else:
+            row_del.append(i)
+    row = np.delete(row, row_del, 0)
+    return row
+
+
