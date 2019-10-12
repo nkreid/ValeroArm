add4d569f5cc5a6a9156f8e39da0e58dfa267400
Koby Reid <kreidpac@gmail.com>
2019-10-11 17:15:41 -0700

    Finalized PossibleRoutes script

--- PossibleScaledRoutes.py
+++ PossibleScaledRoutes.py
@@ -1,44 +1,51 @@
-from FFS import ffs
 import numpy as np
-from OptiFFS import optiFFS
 import itertools
 
 
-def possible_matrix_values(max, step):
+# This function creates a list of possible steps
+def scale_list(max_moment_arm, step):
 	values = [0]
 	x = 0
-	for n in range(int(max/step)):
+	for n in range(int(max_moment_arm/step)):
 		x += step
 		values.append(x)
 	values = np.array(values)
 	return values
 
 
-def iterate_matrix(size, values):
+def iterate_matrix(size, scales):
 	flat_matrix = np.zeros(size).flatten()
-	poss_matrices = list(itertools.product(values.tolist(), repeat=len(flat_matrix)))
+	poss_matrices = list(itertools.product(scales.tolist(), repeat=len(flat_matrix)))
 	# for n in range(len(poss_matrices)):
 	# 	print(poss_matrices[n])
 	return np.array(poss_matrices)
 
-poss_values = possible_matrix_values(1, 0.2)
 
-np.set_printoptions(threshold=np.sys.maxsize)  # Prints un-truncated arrays
+# This function takes in unscaled routes, and their scaling matrices, and outputs a 3d array of all possibilities
+def scaled_routes(possible_routes, scales):
+	scaled_matrix_list = []
+	for i in possible_routes:
+		for n in range(len(iterate_matrix((2,3), scales))):
+			scale_matrix = np.array((iterate_matrix((2,3), scales)[n]))
+			new_matrix = i * scale_matrix.reshape((2,3))
+			scaled_matrix_list.append(new_matrix)
+	new_matrix_3d = np.stack(scaled_matrix_list)
+	return new_matrix_3d
 
-routes = np.load('PossibleRoute.npy')
-for i in routes:
-	for n in range(len(iterate_matrix((2,3), poss_values)[0:10])):
-		scale_matrix = np.array((iterate_matrix((2,3), poss_values)[20000+n]))
-		new_matrix = i* scale_matrix.reshape((2,3))
-		print(new_matrix)
-# routes = []
-# radii = []
-# for i in data:
-#     try:
-#         f = optiFFS(i)
-#     except:
-#         f = 'Error'
-#     routes.append(i)
-#     radii.append(f)
-# print(routes)
-# print(radii)
+
+def save_routes(name, routes):
+	np.save(name, routes)
+
+# np.set_printoptions(threshold=np.sys.maxsize)  # Prints un-truncated arrays
+
+# Parameters for scaling, Maximum moment arm of 1 implies all values are normalized by the largest moment arm
+Scales = scale_list(1, 0.1)
+
+# Possible unscaled routes from prior code
+unscaled_routes = np.load('PossibleRoute.npy')
+
+scaled_routes = scaled_routes(unscaled_routes, Scales)
+
+save_routes('Normalized_Routes', scaled_routes)
+
+
