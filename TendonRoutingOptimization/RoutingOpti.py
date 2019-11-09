f6d8aa9c9916e3560ba33567fdbd22ce6b9c9017
Koby Reid <kreidpac@gmail.com>
2019-11-09 14:14:35 -0800

    fixed code, added save

--- TendonRoutingOptimization/RoutingOpti.py
+++ TendonRoutingOptimization/RoutingOpti.py
@@ -8,6 +8,7 @@ from random import randint
 import pprint
 from fast_ffs import fast_ffs
 from matplotlib import pyplot as plt
+import pickle
 
 
 def optiFFS(rout, angle_step):
@@ -81,14 +82,24 @@ b = poss_rows([-1, 0, 1], 3)
 # np.save('PossibleRoutes',possible_routes(a,b))
 
 
+def save_obj(obj, name):
+    with open('obj/'+ name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+
+def load_obj(name):
+    with open('obj/' + name + '.pkl', 'rb') as f:
+        return pickle.load(f)
+
+
 def status_update(sample, percent_print):
     update = int(sample * percent_print/100)
     return update
 
 
 def random_optimization(sample, angle_step, update, test_order):
-    top_dict = {'Start1': 0, 'Start2': 0, 'Start3': 0, 'Start4': 0, 'Start5': 0,
-                'Start6': 0, 'Start7': 0, 'Start8': 0, 'Start9': 0, 'Start10': 0}
+    test_keys = list(range(100))
+    top_dict = {el:0 for el in test_keys}
     for i in range(sample):
         route = np.array(data[test_order[i]]).reshape((2,3))
         try:
@@ -105,10 +116,10 @@ def random_optimization(sample, angle_step, update, test_order):
 
 
 def sequential_optimization(sample, angle_step, update):
-    top_dict = {'Start1': 0, 'Start2': 0, 'Start3': 0, 'Start4': 0, 'Start5': 0,
-                'Start6': 0, 'Start7': 0, 'Start8': 0, 'Start9': 0, 'Start10': 0}
+    test_keys = list(range(100))
+    top_dict = {el:0 for el in test_keys}
     for i in range(sample):
-        route = np.array(data[i+200000]).reshape((2,3))
+        route = np.array(data[i]).reshape((2,3))
         try:
             f = optiFFS(route, angle_step)
         except:
@@ -119,6 +130,7 @@ def sequential_optimization(sample, angle_step, update):
         top_dict.pop(worst_key[0])
         if i % update == 0:
             print("You are " + str(i*Status_update_percent/update_freq) + " percent done." )
+            save_obj(top_dict, 'temp_top_routes')
     return top_dict
 
 
@@ -126,29 +138,37 @@ def sequential_optimization(sample, angle_step, update):
 data = np.load(os.path.expanduser("~/Downloads/Normalized_Routes_v2.npy"))
 
 # Optimization Parameters
-sample = len(data)                   # Max sample is len(data) = 4,348,472
+sample = 1000                   # Max sample is len(data) = 4,348,472
 Status_update_percent = 1      # Percentages to be notified at during process
 angle_step = 20                 # Angle to step through in the optiFFS function
 update_freq = status_update(sample, Status_update_percent)
 
+# Random Optimization
+random_1000 = np.random.randint(0, high=4348472, size=1000)
+start_time = time.time()
+best_routes = random_optimization(sample, angle_step, update_freq, random_1000)
+print("This program took ", time.time() - start_time, "seconds to run " + str(sample) +
+      " random matrices from the list.\n \nHere are the hundred best matrices:"
+      )
+# Create a list of tuples sorted by index 1 i.e. value field
+listofTuples = sorted(best_routes.items() ,  key=lambda x: x[1])
 
-# # Random Optimization
-# random_1000 = np.random.randint(0, high=4348472, size=1000)
-# start_time = time.time()
-# best_routes = random_optimization(sample, angle_step, update_freq, random_1000)
-# print("This program took ", time.time() - start_time, "seconds to run " + str(sample) +
-#       " random matrices from the list.\n \nHere are the ten best matrices:"
-#       )
-# pprint.pprint(best_routes)
-
-
-# Sequential Optimization
-start_time2 = time.time()
-best_routes = sequential_optimization(sample, angle_step, update_freq)
-print("This program took ", time.time() - start_time2, "seconds to run " + str(sample) +
-      " sequential matrices from the list.\n" +
-      "\nHere are the ten best matrices:")
-pprint.pprint(best_routes)
+# Iterate over the sorted sequence
+for elem in listofTuples :
+    print(elem[0] , " ::" , elem[1] )
 
 
+# # Sequential Optimization
+# start_time2 = time.time()
+# best_routes = sequential_optimization(sample, angle_step, update_freq)
+# save_obj(best_routes, 'best_routes')
+# print("This program took ", time.time() - start_time2, "seconds to run " + str(sample) +
+#       " sequential matrices from the list.\n" +
+#       "\nHere are the hundred best matrices:")
+# # Create a list of tuples sorted by index 1 i.e. value field
+# listofTuples = sorted(best_routes.items() ,  key=lambda x: x[1])
+#
+# # Iterate over the sorted sequence
+# for elem in listofTuples :
+#     print(elem[0] , " ::" , elem[1] )
 
