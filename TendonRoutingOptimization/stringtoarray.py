eb6f1c3ed4da874f88bc2ca33e82db321184df08
Koby Reid <kreidpac@gmail.com>
2019-11-11 00:03:30 -0800

    string2array

---
+++ TendonRoutingOptimization/stringtoarray.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pickle
+
+def load_obj(name):
+	with open('obj/' + name + '.pkl', 'rb') as f:
+		return pickle.load(f)
+
+
+def string2array(string):
+	x = string.replace('[', '')
+	x = x.replace(']', '')
+	x = x.replace('\n', '')
+	a = ' '.join(x.split())
+	return a
+
+
+def keys_return(my_dict):
+	a = list(my_dict.keys())
+	return a
+
+
+def pkl2npy_file(pkl_file):
+	best_array_list = []
+	unpacked = load_obj(pkl_file)
+	best_keys = keys_return(unpacked)
+	for i in range(len(best_keys)):
+		arr = string2array(best_keys[i])
+		best_array_list.append(arr)
+	best_routes = np.vstack(best_array_list)
+	np.save(pkl_file, best_routes)
+
+pkl2npy_file('best_routes')
